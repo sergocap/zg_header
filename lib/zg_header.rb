c5e95541b2e4ff39d8b3259ae4d272fe33525ef1
sergocap <systemofadown.2013@yandex.ru>
2017-02-06 22:34:09 +0700

    change path for sites

--- lib/zg_header.rb
+++ lib/zg_header.rb
@@ -3,7 +3,7 @@ require "zg_header/version"
 module ZgHeader
   class Engine < ::Rails::Engine
     def self.sites
-      @sites ||= YAML.load_file(File.expand_path('../config/sites.yml', __FILE__))
+      @sites ||= YAML.load_file(File.expand_path('../../config/sites.yml', __FILE__))
     end
   end
 end
